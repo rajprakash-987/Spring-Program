7bd31cbcc69859ef2c2f17a0838d640cffa3ff82
rajprakash parhi <rajprakashparhi987@gmail.com>
2025-10-16 07:55:26 +0530

    my third spring program

---
+++ IOCProj03-FactoryPattern-Corejava-bike/src/com/nt/comps/BulletBike.java
@@ -0,0 +1,9 @@
+package com.nt.comps;
+
+public class BulletBike implements Bike {
+	@Override
+	public void drive() {
+		System.out.println("BulletBike.drive()::driving BulletBike(Royal-Enfield)");
+	}
+
+}
---
+++ IOCProj03-FactoryPattern-Corejava-bike/src/com/nt/comps/ElectricBike.java
@@ -0,0 +1,8 @@
+package com.nt.comps;
+
+public class ElectricBike implements Bike {
+	@Override
+ public void drive() {
+		System.out.println("ElectricBike.drive()::driving ElectricBike(Ola-Ather)");
+ }
+}
