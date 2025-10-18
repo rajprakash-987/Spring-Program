2b3952cff5882be4a7d64d1bb92c5bbd9e9d60b5
rajprakash parhi <rajprakashparhi987@gmail.com>
2025-10-18 08:40:17 +0530

    sending xml as response

---
+++ BootRestproj04-RestAPI-SendingXMLasResponse2/src/main/java/com/nt/BootRestproj03RestApiSendingJsoNasResponseApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRestproj03RestApiSendingJsoNasResponseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootRestproj03RestApiSendingJsoNasResponseApplication.class, args);
+	}
+
+}
